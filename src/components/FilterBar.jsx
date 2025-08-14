d8c2bd7641d6c4320956a2a1674165eb64bc78d9
Thomas Gheerbrant <thomas.gheerbrant@live.fr>
2025-08-14 20:51:13 +0200

    v0.8 Filtre complet avec un temps de retard et le message d'abscence de résultat qui ne s'affiche pas

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,25 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Row, Button, Col, Form } from 'react-bootstrap'
 
 const FilterBar = (props) => {
-    const [filters, setFilters] = useState({authorOrTitle: "", genre: "", year: ""});
     const [filterButton, setFilterButton] = useState("Filtrer");
+    const [filters, setFilters] = useState({ authorOrTitle: "", genre: "", year: "" });
 
     const handleFilterButton = () => {
         if (filterButton === "Filtrer") setFilterButton("Masquer les filtres");
-        else setFilterButton("Filtrer")
+        else setFilterButton("Filtrer");
     }
-
-    const handleAuthorTitleChange = () => {
-        setFilters({...filters, authorOrTitle: e.target.value})
+    const handleAuthorTitleChange = (e) => {
+        setFilters({ ...filters, authorOrTitle: e.target.value });
+        props.transmitFilterToBookList(filters);
     }
-    const handleGenreChange = () => {
-        setFilters({...filters, genre: e.target.value})
+    const handleGenreChange = (e) => {
+        if (e.target.value != "Tous les genres") {
+            setFilters({ ...filters, genre: e.target.value });
+        } else { setFilters({ ...filters, genre: "" }) }
+        props.transmitFilterToBookList(filters);
     }
-    const handleYearChange = () => {
-        setFilters({...filters, year: e.target.value})
+    const handleYearChange = (e) => {
+        if (e.target.value != "Tous les années") {
+            setFilters({ ...filters, year: e.target.value });
+        } else { setFilters({ ...filters, year: "" }) }
+        props.transmitFilterToBookList(filters);
     }
-
+    const clearFilters = () => {
+        setFilters({ authorOrTitle: "", genre: "", year: "" });
+        props.transmitFilterToBookList(filters);
+    }
+    useEffect(() => {
+        const timer  = setInterval(() => {}, 500);
+        return () => clearInterval(timer);
+    }, [filters])
     return (
         <>
 
@@ -39,15 +52,15 @@ const FilterBar = (props) => {
                                 <Form.Group>
                                     <Form.Label htmlFor='titleAuthorSearch'>Recherche</Form.Label>
                                     <Form.Control type="text" id='titleAuthorSearch' placeholder="rechercher par titre ou auteur"
-                                        onChangeCapture={handleAuthorTitleChange} defaultValue={""}/>
+                                        onChangeCapture={handleAuthorTitleChange} defaultValue="" />
                                 </Form.Group>
                             </Col>
                             <Col md={3}>
                                 <Form.Group>
                                     <Form.Label htmlFor='byGenreSearch'>Filtrer par genre</Form.Label>
-                                    <Form.Select id='byGenreSearch' onChange={handleGenreChange} defaultValue={""}>
+                                    <Form.Select id='byGenreSearch' onChange={handleGenreChange} defaultValue="Tous les genres">
                                         <option>Tous les genres</option>
-                                        {props.genresList.map((genre,index) => ( <option key={index} value={genre}>{genre}</option>))}
+                                        {props.genresList.map((genre, index) => (<option key={index}>{genre}</option>))}
                                     </Form.Select>
                                 </Form.Group>
 
@@ -55,15 +68,15 @@ const FilterBar = (props) => {
                             <Col md={3}>
                                 <Form.Group>
                                     <Form.Label htmlFor='byYearSearch'>Filtrer par année</Form.Label>
-                                    <Form.Select id='byYearSearch' onChange={handleYearChange} defaultValue={""}>
+                                    <Form.Select id='byYearSearch' onChange={handleYearChange} defaultValue="" >
                                         <option>Toutes les années</option>
-                                        {props.yearsList.map(year => ( <option key={Number(year)} value={year}>{year}</option>))}
+                                        {props.yearsList.map(year => (<option key={Number(year)}>{year}</option>))}
                                     </Form.Select>
                                 </Form.Group>
 
                             </Col>
                             <Col md={3}>
-                                <Button variant='secondary'>Réinitialiser les filtres</Button>
+                                <Button type='reset' variant='secondary' onClick={clearFilters}>Réinitialiser les filtres</Button>
                             </Col>
                         </Row>
                     </Container>
--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -8,8 +8,9 @@ const BookList = () => {
     const [biblio, setBiblio] = useState(null);
     const [chargement, setChargement] = useState(false);
     const [error, setError] = useState(null);
-    const [filter, setFilter] = useState(["", "", ""]);
     const [toBeFiltered, setToBeFiltered] = useState({ authorsAndTitles: [], genres: [], years: [] })
+    const [filters, setFilters] = useState({ authorOrTitle: "", genre: "", year: "" });
+    const [filteredBookList, setFilteredBookList] = useState([]);
 
     const getAll = () => {
         setChargement(true);
@@ -17,9 +18,9 @@ const BookList = () => {
 
         axios.get("http://localhost:3000/books")
             .then(response => {
-                let authorsAndTitlesList =[];
-                let genresList =[];
-                let yearsList =[];
+                let authorsAndTitlesList = [];
+                let genresList = [];
+                let yearsList = [];
                 response.data.forEach(book => {
                     if (!authorsAndTitlesList.includes(book.titre)) authorsAndTitlesList.push(book.titre);
                     if (!authorsAndTitlesList.includes(book.auteur)) authorsAndTitlesList.push(book.auteur);
@@ -28,7 +29,7 @@ const BookList = () => {
                     if (!yearsList.includes(bookDate[0])) yearsList.push(bookDate[0]);
                     book.date = `${bookDate[2]}-${bookDate[1]}-${bookDate[0]}`
                 });
-                setToBeFiltered({ authorsAndTitles: authorsAndTitlesList, genres: genresList, years: yearsList })
+                setToBeFiltered({ authorsAndTitles: authorsAndTitlesList, genres: genresList, years: yearsList.toSorted() })
                 setBiblio([...response.data])
             })
             .catch(erreur => {
@@ -38,15 +39,21 @@ const BookList = () => {
             .finally(() => { setChargement(false); });
     }
 
-    const handleFilter = (dataFromFilterBar) => {
-        return setFilter([...dataFromFilterBar])
-    }
-
     useEffect(() => getAll(), []);
+    useEffect(() => {
+        if (!biblio) return;
+        setFilteredBookList(biblio.filter(book => (
+            (filters.authorOrTitle === "" || book.auteur.includes(filters.authorOrTitle)
+                || book.titre.includes(filters.authorOrTitle))
+            && (filters.genre === "" || book.genre === filters.genre)
+            && (filters.year === "" || book.date.split("-")[2] === filters.year)
+        )))
+    }, [filters, biblio])
+
     return (
         <>
             <Container fluid="xxl" style={{ backgroundColor: "grey", padding: 0 }}>
-                <FilterBar transmitFilterToBookList={handleFilter} genresList={toBeFiltered.genres} yearsList={toBeFiltered.years} />
+                <FilterBar transmitFilterToBookList={setFilters} genresList={toBeFiltered.genres} yearsList={toBeFiltered.years} />
                 <Row className='px-2'>
                     {chargement && (
                         <div className='spinner-container'>
@@ -55,11 +62,12 @@ const BookList = () => {
                         </div>
                     )}
                     {error && (<p className='fw-bold'>{error}</p>)}
-                    {biblio && !chargement && !error && biblio.map(book => (
-                        <Col md={3} key={book.id} className='my-2'>
-                            <BookCard thebook={book} id={book.id} />
-                        </Col>
-                    ))}
+                    {filteredBookList == [] && (<p className='fw-bold'>Aucun livre ne correspond à votre recherche</p>)}
+                    {biblio && !chargement && !error && filteredBookList != [] && filteredBookList.map(book =>
+                    (<Col md={3} key={book.id} className='my-2'>
+                        <BookCard thebook={book} id={book.id} />
+                    </Col>)
+                    )}
                 </Row>
             </Container>
         </>
